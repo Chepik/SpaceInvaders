12687f371bb219398917a6c014eaaa3e3975440a
maks <maks.mephi@gmail.com>
2016-12-05 23:31:35 +0300

    add Globals::SettingsFileName

--- src/constants.cpp
+++ src/constants.cpp
@@ -5,3 +5,5 @@ constexpr float Constants::PI;
 
 int Globals::Height = 768;
 int Globals::Width = 1024;
+
+std::string Globals::SettingsFileName = "settings.json";
--- src/constants.hpp
+++ src/constants.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 struct Constants
 {  
   constexpr static float kEps = 1e-5;
@@ -10,4 +12,5 @@ struct Globals
 {
   static int Height;
   static int Width;
+  static std::string SettingsFileName;
 };
