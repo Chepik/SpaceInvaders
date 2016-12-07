05f22101fcf6853379545ef1e813003331f357e7
maks <maks.mephi@gmail.com>
2016-12-07 16:08:25 +0300

    add Application class to catch an exception

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,16 @@
 #include "mainwindow.hpp"
 #include "except.hpp"
 #include "images.hpp"
+#include "application.hpp"
 
 int main(int argc, char ** argv)
 {
-  QApplication a(argc, argv);
+  Application a(argc, argv);
 
   QSurfaceFormat format;
   format.setDepthBufferSize(24);
   format.setStencilBufferSize(8);
   //  QSurfaceFormat::setDefaultFormat(format);
-
-  try
-  {
-    Images::Instance().LoadImages();
-  }
-  catch (CantLoadImagesException const & ex)
-  {
-    qDebug() << ex.what();
-
-    return 1;
-  }
 
   MainWindow w;
   w.show();
